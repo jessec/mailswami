c774ac8708929331e4a58ef5c1ba85ef8891f345
Jesse <robert@prospecting.in>
2021-08-08 18:58:35 +0700

    cc

---
+++ public/js/table.js
@@ -0,0 +1,111 @@
+class Table {
+  constructor() {
+      console.log("init table");
+  }
+  
+  createJsonTable(serverId, jsonArray){
+      this.removeJsonTable(serverId);
+      var tbl = document.createElement("table");
+      var tblBody = document.createElement("tbody");
+      for(var j = 0; j < jsonArray.length; j++) {
+          var trObj = jsonArray[j];
+          if(j == 0){
+              var row = document.createElement("tr");
+              for (var key of Object.keys(trObj)) {
+                  var cell = document.createElement("td");
+                  cell.classList = "table-th";  
+                  cell.style.border = '1px solid black';  
+                  cell.style.padding = '3px'; 
+                  if(cronManager.hiddenColoms.includes(key)){
+                      cell.classList = "hidden-json-field"; 
+                      cell.style.display = "none";
+                  }
+                  if(cronManager.formatColoms.includes(key)){
+                      tdObj = this.formatColom(key+"_th",tdObj);
+                  }                        
+                  var cellText = document.createTextNode(key);
+                  cell.appendChild(cellText);
+                  row.appendChild(cell);   
+              }
+              tblBody.appendChild(row);                    
+          }
+          var row = document.createElement("tr");
+          for (var key of Object.keys(trObj)) {
+              var tdObj = trObj[key];
+              var cell = document.createElement("td");
+              cell.style.border = '1px solid black';  
+              cell.style.padding = '3px'; 
+              var tdId = serverId+"_"+j+"_"+key;
+              cell.setAttribute("id", tdId);  
+              if(cronManager.editableColoms.includes(key)){
+                  cell.classList = "json-field"; 
+              }
+              if(cronManager.hiddenColoms.includes(key)){
+                  cell.classList = "hidden-json-field"; 
+                  cell.style.display = "none";
+              }
+              if(cronManager.formatColoms.includes(key)){
+                  tdObj = this.formatColom(key,tdObj);
+              }
+              //var cellText = document.createTextNode(tdObj);
+              //cell.appendChild(cellText);
+              //row.appendChild(cell);  
+              
+              if (!this.isElement(tdObj)){
+                  var cellText = document.createTextNode(tdObj);
+                  cell.appendChild(cellText);
+                  row.appendChild(cell);  
+              }else{
+                  cell.appendChild(tdObj);
+                  row.appendChild(cell);  
+              }
+
+              
+          }
+          tblBody.appendChild(row);
+      }
+      tbl.appendChild(tblBody); 
+      tbl.style.borderCollapse = 'collapse';  
+      tbl.setAttribute("id", serverId+"-table");   
+      return tbl;
+  }
+  
+  removeJsonTable(serverId){
+      if(document.querySelector('#'+serverId+"-table")){
+          document.querySelector('#'+serverId+"-table").parentNode.remove();
+      }           
+  }
+  
+  isElement(element) {
+      return element instanceof Element || element instanceof HTMLDocument;  
+  }
+   
+  formatColom(key,tdObj){
+      switch(key) {
+      case "command":
+          return tdObj.replace(/^.*[\\\/]/, '').replace(".sh","");
+        break;
+      case "state":
+          var val = tdObj;
+          var div1 = document.createElement("div");
+          var span1 = document.createElement("span");
+          var cellText = document.createTextNode(val);
+          span1.appendChild(cellText);
+          span1.style.padding = '4px';
+          span1.classList = "state"; 
+          div1.appendChild(span1);
+          var div2 = document.createElement("div");
+          var span2 = document.createElement("span");
+          span2.style.padding = '4px';
+          span2.classList = "state"; 
+          var cellText2 = document.createTextNode("delete");
+          span2.appendChild(cellText2);
+          div1.appendChild(span2);
+          return div1;
+        break;
+      default:
+    }
+  } 
+  
+  
+}
