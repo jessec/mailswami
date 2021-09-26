b1009b11fd6a1fbd166c488255ff2d19e3f849dd
Jesse <robert@prospecting.in>
2021-09-26 23:18:58 +0700

    cc

--- save.sh
+++ save.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-cd /var/www/html/site-mailswami/ ; 
-git add -A ; git commit -m cc; git push --force ; 
-cd _site/ ; 
-git add -A ; git commit -m cc; git push --force ; 
+cp -avr /opt/platform-server/public/* /var/www/html/site-mailswami/public/
+
+cd /var/www/html/site-mailswami/
+git add -A
+git commit -m cc
+git push --force
+cd _site/
+git add -A
+git commit -m cc
+git push --force
 cd /var/www/html/site-mailswami/
