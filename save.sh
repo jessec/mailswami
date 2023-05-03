6058d2c89faef1db64fc7ce75865cbcef2ca653a
thehunt100 <robertjessecooke@gmail.com>
2023-05-03 14:22:17 +0700

    cc

--- save.sh
+++ save.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
 cp -avr /opt/platform-server/public/* /var/www/html/site-mailswami/public/
-cd /var/www/html/site-mailswami/
-git add -A
-git commit -m cc
-git push --force
-cd _site/
-git add -A
-git commit -m cc
-git push --force
-cd /var/www/html/site-mailswami/
+cd /var/www/html/site-mailswami/ ; git add -A ;  git commit -m cc;  git push --force ; cp -avr /var/www/html/site-mailswami/_site/* /var/www/html/mailswami/ ; cd /var/www/html/mailswami/ ;  git add -A ;  git commit -m cc;  git push --force ;  
+
